210338bf182b1b8a56b4d9d26a6789b0be9462ee
Seoyoung Kim <zinna@Zinna-Kim.local>
2025-04-25 14:46:58 -0700

    connecting rules

--- page.js
+++ page.js
@@ -1,4 +1,9 @@
-//Starting game button 
+//Starting game button
 document.getElementById("play-button").onclick = function () {
-    location.href = "/main-page/game.html";
+  location.href = "/main-page/game.html";
+};
+
+//Rules button
+document.querySelector(".rule-page-container__rules").onclick = function () {
+  location.href = "/rule-page/rule.html";
 };
