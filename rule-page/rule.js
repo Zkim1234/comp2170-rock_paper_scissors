f4dfd49535aa9eeba8995d1e2d6737491c64f24c
Denise <daquino10@my.bcit.ca>
2025-04-29 23:49:34 -0700

    add link to play and go back

---
+++ rule-page/rule.js
@@ -0,0 +1,6 @@
+// Let's Play button functionality
+document
+  .querySelector(".play-or-back__buttons__play")
+  .addEventListener("click", () => {
+    location.href = "main-page/game.html";
+  });
