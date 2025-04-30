2f87fa30caf7bd59795cb4a567270b5cba1536b9
Denise <daquino10@my.bcit.ca>
2025-04-29 23:47:20 -0700

    remove js content

--- page.js
+++ page.js
@@ -1,9 +1,1 @@
-//Starting game button
-document.getElementById("play-button").onclick = function () {
-  location.href = "/rock_paper_scissors-COMP2170/main-page/game.html";
-};
 
-//Rules button
-document.querySelector(".rule-page-container__rules").onclick = function () {
-  location.href = "/rock_paper_scissors-COMP2170/rule-page/rule.html";
-};
